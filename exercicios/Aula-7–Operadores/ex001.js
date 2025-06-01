1018bab5e47b7ecf0770627563ada272f25ec2eb
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-06-01 10:43:16 -0400

    Create ex001.js

---
+++ exercicios/Aula-7–Operadores/ex001.js
@@ -0,0 +1,16 @@
+var n = 3 + 2
+var n = 3 - 2
+var n = 3 % 2
+var n = 3 / 2
+var n = 3 * 2
+var n = 3 ** 2 // esses são todos os operadores básicos do js
+
+// Também podemos usar ShortHands no Js, como essas nos operadores
+
+n += 3
+n -= 3
+n %= 3
+n /= 3
+n *= 3
+n **= 3 // ShortHands como essas nos ajudam a ter muito mais produtividade no desenvolvimento.
+
