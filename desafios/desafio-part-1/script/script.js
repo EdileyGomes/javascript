9a69ad470c0ee628996bbf23f9ad855285d64d18
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-06-07 17:21:01 -0400

    2

---
+++ desafios/desafio-part-1/script/script.js
@@ -0,0 +1,24 @@
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 2
+    var body = document.querySelector('body')
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA
+        img.src = '../desafio-part-1/img/sla.JPG' 
+        body.style.backgroundColor = 'Blue'
+    }else if (hora >= 12 && hora < 18){
+        //BOA TARDE
+        img.src = './img/pexels-marek-piwnicki-3907296-19466248.jpg'
+        body.style.backgroundColor = 'red'
+    }else {
+        //BOA NOITE
+        img.src = './img/pexels-edotommo99-32459069.jpg'
+        body.style.backgroundColor = 'black'
+    }
+
+}
+
