1b0c8aeeb9735e2d7962d87d066c59fb5edced09
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-06-06 23:05:11 -0400

    Update ex013.js

--- exercicios/Aula-12-Condições-(Parte 2)/ex013.js
+++ exercicios/Aula-12-Condições-(Parte 2)/ex013.js
@@ -14,7 +14,7 @@ var diaSem = agora.getDay()
 console.log(diaSem)
 switch(diaSem) {
     case 0:
-        console.log('Domingo')
+        console.log('Domingoo')
         break
     case 1:
         console.log('Segunda-Feira')
