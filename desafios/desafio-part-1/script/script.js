97cc567b2ddc0b683a0930348aff5d7336dd02ef
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-06-07 17:23:05 -0400

    2

--- desafios/desafio-part-1/script/script.js
+++ desafios/desafio-part-1/script/script.js
@@ -2,8 +2,7 @@ function carregar() {
     var msg = document.getElementById('msg')
     var img = document.getElementById('imagem')
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 2
+    var hora = data.getHours()
     var body = document.querySelector('body')
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
